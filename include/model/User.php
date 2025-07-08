227b41fdaf1950b982b3733dc767497c543e2ba5
Gabriele2903 <83863969+Gabriele2903@users.noreply.github.com>
2025-07-08 18:31:24 +0200

    Fix aggiornamento 10

--- include/model/User.php
+++ include/model/User.php
@@ -2,11 +2,11 @@
 
 class User{
     private ?int $id;
-    private string $name;
-    private string $surname;
-    private string $email;
-    private string $password;
-    private string $role;
+    private ?string $name;
+    private ?string $surname;
+    private ?string $email;
+    private ?string $password;
+    private ?string $role;
     private ?string $urlImage;
     private ?string $phoneNumber;
 
@@ -26,24 +26,24 @@ class User{
 
     // Getter
     public function getId(): ?int {return $this->id;}
-    public function getName(): string {return $this->name;}
-    public function getSurname(): string {return $this->surname;}
-    public function getEmail(): string {return $this->email;}
-    public function getPassword(): string {return $this->password;}
-    public function getRole(): string {return $this->role;}
-    public function getImage(): string {return $this->urlImage;}
-    public function getPhoneNumber(): string {return $this->phoneNumber;}
+    public function getName(): ?string {return $this->name;}
+    public function getSurname(): ?string {return $this->surname;}
+    public function getEmail(): ?string {return $this->email;}
+    public function getPassword(): ?string {return $this->password;}
+    public function getRole(): ?string {return $this->role;}
+    public function getImage(): ?string {return $this->urlImage;}
+    public function getPhoneNumber(): ?string {return $this->phoneNumber;}
 
 
     // Setter
     public function setId(?int $id) { $this->id = $id; }
-    public function setName(string $name) { $this->name = $name; }
-    public function setSurname(string $surname) { $this->surname = $surname; }
-    public function setEmail(string $email) { $this->email = $email; }
-    public function setPassword(string $password) { $this->password = $password; }
-    public function setRole(string $role) { $this->role = $role; }
-    public function setUrlImage(string $urlImage) { $this->urlImage = $urlImage; }
-    public function setPhoneNumber(string $phoneNumber) { $this->phoneNumber = $phoneNumber; }
+    public function setName(?string $name) { $this->name = $name; }
+    public function setSurname(?string $surname) { $this->surname = $surname; }
+    public function setEmail(?string $email) { $this->email = $email; }
+    public function setPassword(?string $password) { $this->password = $password; }
+    public function setRole(?string $role) { $this->role = $role; }
+    public function setUrlImage(?string $urlImage) { $this->urlImage = $urlImage; }
+    public function setPhoneNumber(?string $phoneNumber) { $this->phoneNumber = $phoneNumber; }
 
     // Other function
     public function toString(): ?string {return $this->name ." ".$this->surname;}
