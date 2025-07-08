1ee53d4ea528cf318c38386f1c40b085948fe3bf
Gabriele2903 <83863969+Gabriele2903@users.noreply.github.com>
2025-07-08 17:54:08 +0200

    Aggiornamento 9

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start();
+
+// Rimuovi tutti i dati associati alla sessione
+session_unset();
+
+// Distruggi la sessione
+session_destroy();
+
+header("Location: index.php");
+exit;
+
+?>
