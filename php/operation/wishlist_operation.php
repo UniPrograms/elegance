d8277bae6c4c125fd6a28337c9195b16e627307f
Lorenzo Feula <feulalorenzo@gmail.com>
2025-07-15 01:59:15 +0200

    wishlist_operation update

--- php/operation/wishlist_operation.php
+++ php/operation/wishlist_operation.php
@@ -24,6 +24,7 @@ $articleDAO = $factory->getArticleDAO();
 // Sposto un articolo dalla wishlist al carrello
 if(isset($_REQUEST["move"])){
     
+    header("Content-Type: application/json;");
     // WishlistItem corrente
     $item = $wishlistItemDAO->getWishlistItemById($_REQUEST["item_id"]);
     
@@ -35,10 +36,22 @@ if(isset($_REQUEST["move"])){
     $new_cart_item = $cartItemDAO->storeItem($new_cart_item);
     
     if($new_cart_item != null){
-       $result = $wishlistItemDAO->deleteItemById($_REQUEST["item_id"]);
-       $ajax_response =  $result ? new AjaxResponse("OK") : new AjaxResponse("NO");
-       echo $ajax_response->build();
-       exit;
+        //Elimina l'articolo dalla wishlist
+        $result = $wishlistItemDAO->deleteItemById($_REQUEST["item_id"]);
+
+        if($result){
+            $cart = $cartDAO->getCartByUserId($_SESSION["id"]);
+            $wishlist = $wishlistDAO->getWishlistByUserId($_SESSION["id"]);
+            $ajax_response = new AjaxResponse("OK");
+            $ajax_response->add("cart_item_size", $cart->getSize());
+            $ajax_response->add("wishlist_item_size", $wishlist->getSize());
+
+            echo $ajax_response->build();
+            exit;
+        }
+        $ajax_response = new AjaxResponse("ERROR");
+        echo $ajax_response->build();
+        exit;
     }
 }
 
@@ -55,21 +68,37 @@ else if(isset($_REQUEST["delete"])){
 
 // Inserisco un articolo nella wishlist
 else if(isset($_REQUEST["store"])){
-   
+        
+    // Se non è stato l'id di un articolo
+    if(!(isset($_REQUEST["product_id"]) && isset($_REQUEST["size_id"]) && isset($_REQUEST["color_id"]))){
+        $ajax_response = new AjaxResponse("ERROR");
+        $ajax_response->add("title_message","Errore del server");
+        $ajax_response->add("text_message","Il server non ha potuto elaborare la richiesta.");
+        echo $ajax_response->build();
+        exit;
+    }
+
+    // Costruisco il nuovo item
+    $new_wishlist_item = new WishlistItem();
+    $new_wishlist_item->setArticle($articleDAO->getArticleByProductSizeColor($_REQUEST["product_id"], $_REQUEST["size_id"], $_REQUEST["color_id"] ));
+    $new_wishlist_item->setWishlist($wishlistDAO->getWishlistByUserId($_SESSION["id"]));
+
+    $new_wishlist_item = $wishlistItemDAO->storeItem($new_wishlist_item); // Lo inserisco nel db
+
+    // Se la store è fallita
+    if($new_wishlist_item == null){
+        $ajax_response = new AjaxResponse("ERROR");
+        $ajax_response->add("title_message","Errore del server");
+        $ajax_response->add("text_message","Non è possibile inserire il prodotto.");
+        echo $ajax_response->build();
+        exit;
+    }
+    
+    $ajax_response = new AjaxResponse("OK");
+
+    echo $ajax_response->build();
+    exit;
+
 }
-
-
-
-// Altrimenti
-else{
-    echo "Non succede assolutamente nulla";
-    // Rimanda ad una pagina di errore
-}
-
-
-
-
-
-
 
 ?>
