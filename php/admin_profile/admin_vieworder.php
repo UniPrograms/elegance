18b9defa55d1f4b8bec727add2d38875533385f2
Lorenzo Feula <feulalorenzo@gmail.com>
2025-07-19 01:02:27 +0200

    Aggiornamento 59

--- php/admin_profile/admin_vieworder.php
+++ php/admin_profile/admin_vieworder.php
@@ -31,6 +31,12 @@ $admin_vieworder_page->setContent("order_number_phone",$order->getAddress()->get
 $admin_vieworder_page->setContent("order_email",$order->getAddress()->getEmail());
 $admin_vieworder_page->setContent("order_customer",$order->getAddress()->getName()." ".$order->getAddress()->getSurname());
 $admin_vieworder_page->setContent("order_price",$order->getPrice());
+$admin_vieworder_page->setContent("order_address_nazione", $order->getAddress()->getNazione());
+$admin_vieworder_page->setContent("order_address_citta", $order->getAddress()->getCitta());
+$admin_vieworder_page->setContent("order_address_via", $order->getAddress()->getVia());
+$admin_vieworder_page->setContent("order_address_civico", $order->getAddress()->getCivico());
+$admin_vieworder_page->setContent("order_address_cap", $order->getAddress()->getCAP());
+$admin_vieworder_page->setContent("order_address_provincia", $order->getAddress()->getProvincia());
 
 // Setto la select per definire gli stati dell'ordine
 $order_status = ["IN_LAVORAZIONE" => "in progress", 
