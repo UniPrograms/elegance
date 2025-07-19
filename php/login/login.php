3cf2808d126f06109d639d1897f53a92e85812c9
Lorenzo Feula <feulalorenzo@gmail.com>
2025-07-19 16:03:13 +0200

    Divertiti

--- php/login/login.php
+++ php/login/login.php
@@ -33,7 +33,7 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
             
         // Se l'utente è un amministratore, allora lo rimanda alla schermata principale
         if(strtoupper($user->getRole()) == "AMMINISTRATORE")  { 
-            header("Location: admin_dashboard.php");
+            header("Location: profile.php");
             exit;
         } 
 
