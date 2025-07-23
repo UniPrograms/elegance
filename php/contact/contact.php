6c67f3b7e49e8f7e0eceb51f3d9f4b35a08979e3
Gabriele2903 <83863969+Gabriele2903@users.noreply.github.com>
2025-07-23 17:49:37 +0200

    Miglioramento 23

--- php/contact/contact.php
+++ php/contact/contact.php
@@ -2,4 +2,11 @@
 
 $contact_page = new Template("skin/contact/contact.html");
 
+$contact_page->setContent("img", "img/my_img/core/capo_che_cuce.png");
+$contact_page->setContent("img_quote", "La moda si fa con ago e filo, ma si indossa con il cuore.");
+$contact_page->setContent("img_quote_author", "Alessandro Giuseppe Casasanta");
+$contact_page->setContent("address", "Via Spineto I, 93");
+$contact_page->setContent("telephone", "+39 393 149 8260");
+$contact_page->setContent("email", "alecas@elegance.com");
+
 ?>
